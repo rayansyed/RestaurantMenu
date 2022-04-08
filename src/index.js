f317f3a13aa309695b2f892cdb209afb700a7fa8
Rayan Syed <45608348+rayansyed@users.noreply.github.com>
2022-04-08 13:58:33 -0400

    Filtering function added

--- src/index.js
+++ src/index.js
@@ -17,32 +17,52 @@ const groupByType = menuItems.reduce((acc, item) => {
 }, {});
 
 function generateItems(arg) {
+  //initialize empty string for HTML
   let items = "";
+
+  //itterate through the array and create HTML with correct data
   for (let i = 0; i < arg.length; i++) {
     //adjust price to 2 decimal places
     var price = parseFloat(arg[i].price).toFixed(2);
 
-    items +=
-      "<li><span class='itemName'>" +
-      arg[i].name +
-      "</span><span class='itemPrice'>$" +
-      price +
-      "</span></li>";
-
-    //adds spice icon to description if menu item is spicy
+    //spicy items have spicyItem class which is used when spicy food checkbox is clicked
+    //spicy items also have an icon next to their title
     if (arg[i].spicy === true) {
       items +=
-        "<li class='itemDesc'>" +
-        arg[i].description +
-        "<span class='spicy'></span>" +
-        "</li>";
+        "<div class='spicyItem'><li><span class='itemName'>" +
+        arg[i].name +
+        "<span class='spicy'></span>";
     } else {
-      items += "<li class='itemDesc'>" + arg[i].description + "</li>";
+      items += "<div><li><span class='itemName'>" + arg[i].name;
     }
+
+    items +=
+      "</span><span class='itemPrice'>$" +
+      price +
+      "</span></li><li class='itemDesc' >" +
+      arg[i].description +
+      "</li></div>";
   }
 
   return items;
 }
+
+window.displaySpicyMenu = function () {
+  var checkbox = document.getElementById("spicyCheck");
+  var spicyMenuItems = document.getElementsByClassName("spicyItem");
+  var i;
+  if (checkbox.checked === false) {
+    for (i = 0; i < spicyMenuItems.length; i++) {
+      console.log(spicyMenuItems[i]);
+      spicyMenuItems[i].style.display = "none";
+    }
+  } else {
+    for (i = 0; i < spicyMenuItems.length; i++) {
+      console.log(spicyMenuItems[i]);
+      spicyMenuItems[i].style.display = "inline";
+    }
+  }
+};
 
 document.getElementById("startersList").innerHTML = generateItems(
   groupByType.starters
